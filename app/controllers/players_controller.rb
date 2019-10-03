24ab33a0536890f35432e48f2c7b88465521218a
Serge Hänni <serge@nyi.ch>
2019-10-03 10:56:28 +0200

    Add players list

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,10 @@
 class PlayersController < ApplicationController
   def index
+    client = Contentful::Client.new(
+      space: 'xgf5e7365iv2',
+      access_token: 'B3mD-MYx05RkKMPuZFylX34ZiX9_M43ltx2diDeO4mM'
+    )
+
+    @players = client.entries(content_type: 'monthlyRanking', order: 'fields.name')
   end
 end
