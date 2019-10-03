81dbf49f5e860af561e7d0f023e0cd3647e072b1
Mischa Steiner <mischa.steiner@simplificator.com>
2019-10-03 11:35:54 +0200

    Add very static show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,10 +1,18 @@
 class PlayersController < ApplicationController
   def index
-    client = Contentful::Client.new(
+    @players = client.entries(content_type: 'monthlyRanking', order: 'fields.name')
+  end
+
+  def show
+    @player = client.entry('SMDbocMBVCITPhfI4OG0m', content_type: 'monthlyRanking', include: 1)
+  end
+
+  private
+  
+  def client
+    Contentful::Client.new(
       space: 'xgf5e7365iv2',
       access_token: 'B3mD-MYx05RkKMPuZFylX34ZiX9_M43ltx2diDeO4mM'
     )
-
-    @players = client.entries(content_type: 'monthlyRanking', order: 'fields.name')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'players#index'
 
-  resources :players, only: :index
+  resources :players, only: [:index, :show]
 end
